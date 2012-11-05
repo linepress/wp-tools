57440be1133ac625c7b65aed6d42671c5d088390
sebastiaandegeus <sebastiaan@hoppinger.com>
2012-11-06 00:23:59 +0100

    indentation, better var names

--- skeleton/settings.php
+++ skeleton/settings.php
@@ -1,12 +1,12 @@
 <?php
 $settings = array(
-	'DB_HOST'         => 'localhost',
-	'DB_NAME'         => '%DBNAME%',
-	'DB_USER'         => '%DBUSER%',
-	'DB_PASSWORD'       => '%DBPASS%',
-	'DOMAIN_CURRENT_SITE'   => '%SITEURL%',
-	'WP_DEBUG'        => TRUE,
-	'ENVIRONMENT'        => 'dev',
-	'ACF_REPEATER'  => "",
-	'ACF_OPTIONS'  => "",
+  'DB_HOST'              => 'localhost',
+  'DB_NAME'              => '%DB_NAME%',
+  'DB_USER'              => '%DB_USER%',
+  'DB_PASSWORD'          => '%DB_PASS%',
+  'DOMAIN_CURRENT_SITE'  => '%DOMAIN_CURRENT_SITE%',
+  'WP_DEBUG'             => TRUE,
+  'ENVIRONMENT'          => 'dev',
+  'ACF_REPEATER'         => '',
+  'ACF_OPTIONS'          => '',
 );
